47201e63a91a2f675ee45e01a8b5a9de7dbcdccd
Manuel Schlund <manuel.schlund@dlr.de>
2019-03-04 10:27:13 +0100

    Improved ECS calculation and added more complex plots

--- esmvaltool/diag_scripts/climate_metrics/ecs.py
+++ esmvaltool/diag_scripts/climate_metrics/ecs.py
@@ -47,6 +47,119 @@ EXP_4XCO2 = {
 }
 
 
+def _get_anomaly_data(tas_data, rtmt_data, dataset, year_idx=None):
+    """Calculate anomaly data for both variables."""
+    project = tas_data[0]['project']
+    exp_4xco2 = EXP_4XCO2[project]
+    paths = {
+        'tas_4x': select_metadata(tas_data, dataset=dataset, exp=exp_4xco2),
+        'tas_pi': select_metadata(tas_data, dataset=dataset, exp='piControl'),
+        'rtmt_4x': select_metadata(rtmt_data, dataset=dataset, exp=exp_4xco2),
+        'rtmt_pi': select_metadata(
+            rtmt_data, dataset=dataset, exp='piControl'),
+    }
+    ancestor_files = []
+    cubes = {}
+    for (key, [path]) in paths.items():
+        ancestor_files.append(path['filename'])
+        cube = iris.load_cube(path['filename'])
+        cube = cube.aggregated_by('year', iris.analysis.MEAN)
+
+        # Linear regression of piControl to account for drift and imbalance
+        if key.endswith('pi'):
+            reg = stats.linregress(cube.coord('year').points, cube.data)
+            cube.data = reg.slope * cube.coord('year').points + reg.intercept
+
+        # Extract correct years
+        if year_idx is not None:
+            cube = cube[year_idx]
+        cubes[key] = cube
+
+    # Check shapes
+    shape = None
+    for cube in cubes.values():
+        if shape is None:
+            shape = cube.shape
+        else:
+            if cube.shape != shape:
+                raise ValueError(
+                    "Expected all cubes of dataset '{}' to have identical "
+                    "shapes, got {} and {}".format(dataset, shape, cube.shape))
+
+    # Subtract linear trend of piControl experiments from abrupt 4xCO2
+    cubes['tas_4x'].data -= cubes['tas_pi'].data
+    cubes['rtmt_4x'].data -= cubes['rtmt_pi'].data
+    return (cubes['tas_4x'], cubes['rtmt_4x'], ancestor_files)
+
+
+def _get_multi_model_mean(cubes, project):
+    """Get multi-model mean for dictionary of cubes."""
+    if not cubes:
+        logger.warning("Cannot calculate multi-model since no data was given.")
+        return None
+    mmm_data = []
+    for cube in cubes.values():
+        mmm_data.append(cube.data)
+    cube = cubes[list(cubes.keys())[0]]
+    mmm_data = np.ma.array(mmm_data)
+    mmm_data = np.ma.mean(mmm_data, axis=0)
+    mmm_cube = cube.copy(data=mmm_data)
+    mmm_cube.attributes = {
+        'dataset': 'MultiModelMean',
+        'project': project,
+    }
+    return mmm_cube
+
+
+def calculate_ecs(cfg, description, year_idx=None):
+    """Calculate ECS and plot regression for a certain amount of years."""
+    # Read external file if desired
+    if cfg.get('read_external_file'):
+        (ecs, clim_sens, external_file) = read_external_file(cfg)
+    else:
+        check_input_data(cfg)
+        ecs = {}
+        clim_sens = {}
+        external_file = None
+
+    # Read data
+    input_data = cfg['input_data'].values()
+    (tas_cubes, rtmt_cubes, ancestor_files) = preprocess_data(
+        input_data, description, year_idx)
+
+    # Iterate over all datasets and save ECS and climate sensitivity
+    for (dataset, tas_cube) in tas_cubes.items():
+        logger.info("Processing %s for %s", description, dataset)
+        rtmt_cube = rtmt_cubes[dataset]
+        ancestors = ancestor_files[dataset]
+
+        # Plot ECS regression if desired
+        (reg, path, provenance_record) = _plot_ecs_regression(
+            cfg, dataset, description, tas_cube, rtmt_cube)
+
+        # Provenance
+        if path is not None:
+            provenance_record['ancestors'] = ancestors
+            with ProvenanceLogger(cfg) as provenance_logger:
+                provenance_logger.log(path, provenance_record)
+
+        # Save data
+        if cfg.get('read_external_file') and dataset in ecs:
+            logger.info(
+                "Overwriting external given ECS and climate "
+                "sensitivity for %s", dataset)
+        ecs[dataset] = -reg.intercept / (2 * reg.slope)
+        clim_sens[dataset] = -reg.slope
+
+    # Write data
+    tas_data = select_metadata(input_data, short_name='tas')
+    rtmt_data = select_metadata(input_data, short_name='rtmt')
+    ancestor_files = [d['filename'] for d in tas_data + rtmt_data]
+    if external_file is not None:
+        ancestor_files.append(external_file)
+    write_data(ecs, clim_sens, ancestor_files, description, cfg)
+
+
 def check_input_data(cfg):
     """Check input data."""
     if not variables_available(cfg, ['tas', 'rtmt']):
@@ -69,40 +182,6 @@ def check_input_data(cfg):
                                                       exps))
 
 
-def get_anomaly_data(tas_data, rtmt_data, dataset):
-    """Calculate anomaly data for both variables."""
-    project = tas_data[0]['project']
-    exp_4xco2 = EXP_4XCO2[project]
-    paths = {
-        'tas_4x': select_metadata(tas_data, dataset=dataset, exp=exp_4xco2),
-        'tas_pi': select_metadata(tas_data, dataset=dataset, exp='piControl'),
-        'rtmt_4x': select_metadata(rtmt_data, dataset=dataset, exp=exp_4xco2),
-        'rtmt_pi': select_metadata(
-            rtmt_data, dataset=dataset, exp='piControl'),
-    }
-    ancestor_files = []
-    cubes = {}
-    for (key, [path]) in paths.items():
-        ancestor_files.append(path['filename'])
-        cube = iris.load_cube(path['filename'])
-        cube = cube.aggregated_by('year', iris.analysis.MEAN)
-        cubes[key] = cube
-
-    # Substract piControl run from abrupt4xCO2 experiment
-    shape = None
-    for cube in cubes.values():
-        if shape is None:
-            shape = cube.shape
-        else:
-            if cube.shape != shape:
-                raise ValueError(
-                    "Expected all cubes of dataset '{}' to have identical "
-                    "shapes, got {} and {}".format(dataset, shape, cube.shape))
-    cubes['tas_4x'].data -= cubes['tas_pi'].data
-    cubes['rtmt_4x'].data -= cubes['rtmt_pi'].data
-    return (cubes['tas_4x'], cubes['rtmt_4x'], ancestor_files)
-
-
 def get_provenance_record(caption):
     """Create a provenance record describing the diagnostic data and plot."""
     record = {
@@ -115,6 +194,34 @@ def get_provenance_record(caption):
         'themes': ['phys'],
     }
     return record
+
+
+def preprocess_data(input_data, description, year_idx=None):
+    """Read input datasets and calculate anomalies and multi-model mean."""
+    tas_data = select_metadata(input_data, short_name='tas')
+    rtmt_data = select_metadata(input_data, short_name='rtmt')
+    tas_cubes = {}
+    rtmt_cubes = {}
+    ancestor_files = {}
+    project = tas_data[0]['project']
+
+    # Calculate anomalies for every dataset
+    for dataset in group_metadata(tas_data, 'dataset'):
+        logger.info("Preprocessing %s for %s", description, dataset)
+        (tas_cube, rtmt_cube, ancestors) = _get_anomaly_data(
+            tas_data, rtmt_data, dataset, year_idx)
+        tas_cubes[dataset] = tas_cube
+        rtmt_cubes[dataset] = rtmt_cube
+        ancestor_files[dataset] = ancestors
+
+    # Calculate multi-model mean
+    logger.info("Calculating multi-model mean")
+    key = '{} MultiModelMean'.format(project)
+    tas_cubes[key] = _get_multi_model_mean(tas_cubes, project)
+    rtmt_cubes[key] = _get_multi_model_mean(rtmt_cubes, project)
+    ancestor_files[key] = [d['filename'] for d in tas_data + rtmt_data]
+
+    return (tas_cubes, rtmt_cubes, ancestor_files)
 
 
 def read_external_file(cfg):
@@ -141,20 +248,22 @@ def read_external_file(cfg):
     return (ecs, clim_sens, filepath)
 
 
-def plot_ecs_regression(cfg, dataset_name, tas_cube, rtmt_cube, reg_stats):
+def _plot_ecs_regression(cfg, dataset_name, description, tas_cube, rtmt_cube):
     """Plot linear regression used to calculate ECS."""
     if not (cfg['write_plots'] and cfg.get('plot_ecs_regression')):
         return (None, None)
-    ecs = -reg_stats.intercept / (2 * reg_stats.slope)
+    reg = stats.linregress(tas_cube.data, rtmt_cube.data)
 
     # Regression line
     x_reg = np.linspace(-1.0, 9.0, 2)
-    y_reg = reg_stats.slope * x_reg + reg_stats.intercept
+    y_reg = reg.slope * x_reg + reg.intercept
 
     # Plot data
+    title = '{} for {}'.format(description, dataset_name)
+    plot_path = get_plot_filename(title.replace(' ', '_'), cfg)
     text = r'r = {:.2f}, $\lambda$ = {:.2f}, F = {:.2f}, ECS = {:.2f}'.format(
-        reg_stats.rvalue, -reg_stats.slope, reg_stats.intercept, ecs)
-    plot_path = get_plot_filename(dataset_name, cfg)
+        reg.rvalue, -reg.slope, reg.intercept,
+        -reg.intercept / (2.0 * reg.slope))
     plot.scatterplot(
         [tas_cube.data, x_reg],
         [rtmt_cube.data, y_reg],
@@ -173,7 +282,7 @@ def plot_ecs_regression(cfg, dataset_name, tas_cube, rtmt_cube, reg_stats):
             'orientation': 'landscape',
         },
         axes_functions={
-            'set_title': dataset_name,
+            'set_title': title,
             'set_xlabel': 'tas / ' + tas_cube.units.origin,
             'set_ylabel': 'rtmt / ' + rtmt_cube.units.origin,
             'set_xlim': [0.0, 8.0],
@@ -188,31 +297,14 @@ def plot_ecs_regression(cfg, dataset_name, tas_cube, rtmt_cube, reg_stats):
     )
 
     # Write netcdf file for every plot
-    tas_coord = iris.coords.AuxCoord(
-        tas_cube.data,
-        **extract_variables(cfg, as_iris=True)['tas'])
-    attrs = {
-        'model': dataset_name,
-        'regression_r_value': reg_stats.rvalue,
-        'regression_slope': reg_stats.slope,
-        'regression_interception': reg_stats.intercept,
-        'climate_sensitivity': -reg_stats.slope,
-        'ECS': ecs,
-    }
-    cube = iris.cube.Cube(
-        rtmt_cube.data,
-        attributes=attrs,
-        aux_coords_and_dims=[(tas_coord, 0)],
-        **extract_variables(cfg, as_iris=True)['rtmt'])
-    netcdf_path = get_diagnostic_filename('ecs_regression_' + dataset_name,
-                                          cfg)
-    io.save_iris_cube(cube, netcdf_path)
+    netcdf_path = _save_evs_cube(tas_cube, rtmt_cube, reg, title, cfg)
 
     # Provenance
     provenance_record = get_provenance_record(
         "Scatterplot between TOA radiance and global mean surface temperature "
-        "anomaly for 150 years of the abrupt 4x CO2 experiment including "
-        "linear regression to calculate ECS for {}.".format(dataset_name))
+        "anomaly for {} of the abrupt 4x CO2 experiment including linear "
+        "regression to calculate ECS for {} (following Andrews et al., "
+        "Geophys. Res. Lett., 39, 2012).".format(description, dataset_name))
     provenance_record.update({
         'plot_file': plot_path,
         'plot_types': ['scatter'],
@@ -221,7 +313,32 @@ def plot_ecs_regression(cfg, dataset_name, tas_cube, rtmt_cube, reg_stats):
     return (netcdf_path, provenance_record)
 
 
-def write_data(ecs_data, clim_sens_data, ancestor_files, cfg):
+def _save_evs_cube(tas_cube, rtmt_cube, reg, title, cfg):
+    """Save ECS cube for a given dataset."""
+    ecs = -reg.intercept / (2.0 * reg.slope)
+    tas_coord = iris.coords.AuxCoord(
+        tas_cube.data,
+        **extract_variables(cfg, as_iris=True)['tas'])
+    attrs = {
+        'model': title,
+        'regression_r_value': reg.rvalue,
+        'regression_slope': reg.slope,
+        'regression_interception': reg.intercept,
+        'climate_sensitivity': -reg.slope,
+        'ECS': ecs,
+    }
+    cube = iris.cube.Cube(
+        rtmt_cube.data,
+        attributes=attrs,
+        aux_coords_and_dims=[(tas_coord, 0)],
+        **extract_variables(cfg, as_iris=True)['rtmt'])
+    netcdf_path = get_diagnostic_filename(
+        'ecs_regression_{}'.format(title.replace(' ', '_')), cfg)
+    io.save_iris_cube(cube, netcdf_path)
+    return netcdf_path
+
+
+def write_data(ecs_data, clim_sens_data, ancestor_files, description, cfg):
     """Write netcdf files."""
     data = [ecs_data, clim_sens_data]
     var_attrs = [
@@ -237,9 +354,14 @@ def write_data(ecs_data, clim_sens_data, ancestor_files, cfg):
         },
     ]
     for (idx, var_attr) in enumerate(var_attrs):
-        path = get_diagnostic_filename(var_attr['short_name'], cfg)
-        io.save_scalar_data(data[idx], path, var_attr)
-        caption = "{long_name} for multiple climate models.".format(**var_attr)
+        filename = '{}_{}'.format(var_attr['short_name'],
+                                  description.replace(' ', '_'))
+        path = get_diagnostic_filename(filename, cfg)
+        attributes = {'Description': description}
+        io.save_scalar_data(data[idx], path, var_attr, attributes=attributes)
+        caption = ("{long_name} for multiple climate models for "
+                   "{description}.".format(
+                       description=description, **var_attr))
         provenance_record = get_provenance_record(caption)
         provenance_record['ancestors'] = ancestor_files
         with ProvenanceLogger(cfg) as provenance_logger:
@@ -248,53 +370,14 @@ def write_data(ecs_data, clim_sens_data, ancestor_files, cfg):
 
 def main(cfg):
     """Run the diagnostic."""
-    input_data = cfg['input_data'].values()
-
-    # Read external file if desired
-    if cfg.get('read_external_file'):
-        (ecs, clim_sens, external_file) = read_external_file(cfg)
-    else:
-        check_input_data(cfg)
-        ecs = {}
-        clim_sens = {}
-        external_file = None
-
-    # Read data
-    tas_data = select_metadata(input_data, short_name='tas')
-    rtmt_data = select_metadata(input_data, short_name='rtmt')
-
-    # Iterate over all datasets and save ECS and climate sensitivity
-    for dataset in group_metadata(tas_data, 'dataset'):
-        logger.info("Processing %s", dataset)
-        (tas_cube, rtmt_cube, ancestor_files) = get_anomaly_data(
-            tas_data, rtmt_data, dataset)
-
-        # Perform linear regression
-        reg = stats.linregress(tas_cube.data, rtmt_cube.data)
-
-        # Plot ECS regression if desired
-        (path, provenance_record) = plot_ecs_regression(
-            cfg, dataset, tas_cube, rtmt_cube, reg)
-
-        # Provenance
-        if path is not None:
-            provenance_record['ancestors'] = ancestor_files
-            with ProvenanceLogger(cfg) as provenance_logger:
-                provenance_logger.log(path, provenance_record)
-
-        # Save data
-        if cfg.get('read_external_file') and dataset in ecs:
-            logger.info(
-                "Overwriting external given ECS and climate "
-                "sensitivity for %s", dataset)
-        ecs[dataset] = -reg.intercept / (2 * reg.slope)
-        clim_sens[dataset] = -reg.slope
-
-    # Write data
-    ancestor_files = [d['filename'] for d in tas_data + rtmt_data]
-    if external_file is not None:
-        ancestor_files.append(external_file)
-    write_data(ecs, clim_sens, ancestor_files, cfg)
+    year_indices = {
+        'all 150 years': None,
+        'first 20 years': slice(None, 20),
+        'last 130 years': slice(20, None),
+    }
+    for (descr, year_idx) in year_indices.items():
+        logger.info("Considering %s for all datasets", descr)
+        calculate_ecs(cfg, descr, year_idx)
 
 
 if __name__ == '__main__':
